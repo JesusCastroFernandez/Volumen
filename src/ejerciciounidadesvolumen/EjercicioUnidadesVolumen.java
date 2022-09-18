66e8bdbdf351ba20d373744460ed1e626862434a
JesusCastroFernandez <castrofernandezjesus@gmail.com>
2022-09-18 09:30:00 +0200

    Volumen
    Unidades

---
+++ src/ejerciciounidadesvolumen/EjercicioUnidadesVolumen.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciounidadesvolumen;
+
+/**
+ *
+ * @author JESUS
+ */
+public class EjercicioUnidadesVolumen {
+
+    /**
+     * Ejercicio de Unidades de Volumen
+     */
+    
+    public enum UnidadVolumen {MILILITRO, CENTILITRO, DECILITRO, LITRO, DECALITRO, HECTOLITRO};
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println(UnidadVolumen.MILILITRO);
+         System.out.println(UnidadVolumen.MILILITRO);
+         System.out.println(UnidadVolumen.CENTILITRO);
+         System.out.println(UnidadVolumen.DECILITRO);
+         System.out.println(UnidadVolumen.LITRO);
+         System.out.println(UnidadVolumen.DECALITRO);
+         System.out.println(UnidadVolumen.HECTOLITRO);
+              
+        
+    }
+    
+}
